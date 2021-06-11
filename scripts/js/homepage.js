938d50d8f08b376f0bcf7ddc2dafc86ce27c4a99
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-06-11 17:22:41 +0100

    Two paths

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -14,10 +14,13 @@ import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 // *=========================================
 // ** GSAP  **
 // *=========================================
+gsap.registerPlugin(CSSRulePlugin, ScrollTrigger, DrawSVGPlugin);
 
 const animationOne = document.querySelector('#Animation_1');
 const animationOnePaths = animationOne.querySelectorAll('path');
-console.log(animationOnePaths);
+
+const animationTwo = document.querySelector('#Animation_2');
+const animationTwoPaths = animationTwo.querySelectorAll('path');
 
 animationOnePaths.forEach((paths) => {
   if (!paths.classList.contains('cls-1')) {
@@ -25,7 +28,11 @@ animationOnePaths.forEach((paths) => {
   }
 });
 
-gsap.registerPlugin(CSSRulePlugin, ScrollTrigger, DrawSVGPlugin);
+animationTwoPaths.forEach((paths) => {
+  if (!paths.classList.contains('cls-1')) {
+    gsap.fromTo(paths, { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 3 });
+  }
+});
 
 // gsap.set('.cls-1', { opacity: 0 });
 // gsap.fromTo('.cls-1', { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 3 });
