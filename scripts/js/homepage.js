54109a4ef3e0b3ac3d0ba3b0c2b00bdbd65ba779
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-06-11 17:58:42 +0100

    Traking dowm paths that need to be flipped

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -16,23 +16,42 @@ import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 // *=========================================
 gsap.registerPlugin(CSSRulePlugin, ScrollTrigger, DrawSVGPlugin);
 
+// *=========================================
+// ** Main Logo  **
+// *=========================================
+
+//* Find which paths run the wrong way and flip them
+//* https://greensock.com/forums/topic/14946-drawsvg-start-from-the-other-end/
+//* https://greensock.com/forums/topic/21132-drawsvg-path-draws-from-right-point-to-left-point-but-i-am-looking-for-left-to-right-have-i-drawn-all-my-paths-the-wrong-direction/?do=findComment&comment=98969&_rid=75370
+
+// const logoPaths = document.querySelectorAll('.main-logo-wrapper path');
+// console.log(logoPaths);
+
+// gsap.fromTo(logoPaths, { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 2 });
+
 const animationOne = document.querySelector('#Animation_1');
 const animationOnePaths = animationOne.querySelectorAll('path');
 
 const animationTwo = document.querySelector('#Animation_2');
 const animationTwoPaths = animationTwo.querySelectorAll('path');
 
-animationOnePaths.forEach((paths) => {
-  if (!paths.classList.contains('cls-1')) {
-    gsap.fromTo(paths, { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 3 });
-  }
+const animationThree = document.querySelector('#Animation_3');
+const animationThreePaths = animationThree.querySelectorAll('path');
+
+const animationFour = document.querySelector('#Animation_4');
+const animationFourPaths = animationFour.querySelectorAll('path');
+
+const animationFive = document.querySelector('#Animation_5');
+const animationFivePaths = animationFive.querySelectorAll('path');
+
+const flipReverse = gsap.timeline({
+  defaults: { duration: 8 },
+  repeat: -1,
 });
 
-animationTwoPaths.forEach((paths) => {
-  if (!paths.classList.contains('cls-1')) {
-    gsap.fromTo(paths, { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 3 });
-  }
-});
-
-// gsap.set('.cls-1', { opacity: 0 });
-// gsap.fromTo('.cls-1', { drawSVG: 0 }, { drawSVG: '100%', delay: 2, duration: 3 });
+flipReverse
+  .fromTo(animationOnePaths, { drawSVG: 0 }, { drawSVG: '100%' })
+  .fromTo(animationTwoPaths, { drawSVG: 0 }, { drawSVG: '100%' })
+  .fromTo(animationThreePaths, { drawSVG: 0 }, { drawSVG: '100%' })
+  .fromTo(animationFourPaths, { drawSVG: 0 }, { drawSVG: '100%' })
+  .fromTo(animationFivePaths, { drawSVG: 0 }, { drawSVG: '100%' });
