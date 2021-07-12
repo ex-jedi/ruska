2b7b9ba6c8a91cbd875aa83b944d94fd5117b90b
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-07-12 12:37:08 +0100

    Logo animation tweaks

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -35,17 +35,18 @@ const animationFourPaths = animationFour.querySelectorAll('path');
 const animationFive = document.querySelector('#Animation_5');
 const animationFivePaths = animationFive.querySelectorAll('path');
 
+const logoImage = document.querySelector('.main-logo-wrapper image');
+console.log(logoImage);
+
 const flipReverse = gsap.timeline({
   defaults: { duration: 1, ease: 'power3.inOut' },
   repeat: -1,
 });
 
 flipReverse
+  .fromTo(logoImage, { opacity: 0 }, { opacity: 1, ease: 'power4.in' })
   .fromTo(animationOnePaths, { drawSVG: 0 }, { drawSVG: '100%' })
   .fromTo(animationTwoPaths, { drawSVG: 0 }, { drawSVG: '100%' })
   .fromTo(animationThreePaths, { drawSVG: 0 }, { drawSVG: '100%' })
   .fromTo(animationFourPaths, { drawSVG: 0 }, { drawSVG: '100%' })
   .fromTo(animationFivePaths, { drawSVG: 0 }, { drawSVG: '100%' });
-
-console.log({ animationTwo });
-console.log({ animationTwoPaths });
