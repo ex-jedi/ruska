e6f01f0ff192e04521f6c6fcd80f9741e1e7fc31
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-07-28 14:27:39 +0100

    Moving body text styles to main.scss

---
+++ about-me.php
@@ -0,0 +1,13 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('heads/main-head'); ?>
+<?php perch_layout('headers/about-me-header'); ?>
+<main class="main-content about-me-main-content" id="main-content">
+	<?php perch_content("Bio"); ?>
+	<?php perch_content("Contact Form"); ?>
+	<?php perch_content("Instagram Feed"); ?>
+</main>
+<?php perch_layout('footers/main-footer'); ?>
+<?php perch_layout('footer-ends/main-footer-end'); ?>
+<!-- TODO: Remove debug -->
+<?php PerchUtil::output_debug(); ?>
+<!-- TODO: Validate HTML -->
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<?php perch_content("Instagram Feed"); ?>
 </main>
 <?php perch_layout('footers/main-footer'); ?>
-<?php perch_layout('footer-ends/main-footer-end'); ?>
+<?php perch_layout('footer-ends/homepage-footer-end'); ?>
 <!-- TODO: Remove debug -->
 <?php PerchUtil::output_debug(); ?>
 <!-- TODO: Validate HTML -->
