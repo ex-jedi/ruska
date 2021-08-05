2084b07e4652071bcbca4bc19f2daa6bf152ef7c
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-05 12:30:47 +0100

    Instafeed for blog

--- blog/index.php
+++ blog/index.php
@@ -3,6 +3,7 @@
 <?php perch_layout('headers/blog-index-header'); ?>
   <main class="blog-main-content blog-index-main-content" id="main-content">
     <?php perch_blog_recent_posts(5); ?>
+    <?php perch_content("Instagram Feed"); ?>
   </main>
 <?php perch_layout('footers/main-footer'); ?>
 <?php perch_layout('footer-ends/blog-footer-end'); ?>
