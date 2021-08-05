67335a80aeed1fad2b451975591ae3257f309bdc
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-05 15:04:22 +0100

    Blog header bg image

--- perch/templates/layouts/headers/blog-index-header.php
+++ perch/templates/layouts/headers/blog-index-header.php
@@ -4,20 +4,20 @@
 	<!-- Cookie Warning -->
 	<?php perch_content("Cookie Warning"); ?>
 	<div class="site-wrapper" itemscope itemtype="http://schema.org/LocalBusiness">
+	<?php perch_content("Header Content"); ?>
 	<header class="header main-header">
-			<?php perch_content("Header Content"); ?>
-			<!-- Skip to main content link  -->
-			<a class="show-on-focus"  href="#main-content">Skip to main content</a>
-			<!-- For SVG logo. Delete if not needed -->
-				<?php perch_content("Hidden Logo"); ?>
-			<div class="main-logo-wrapper">
-				<a class="main-logo-link"  href="/">
-					<?php perch_content("Main Logo"); ?>
-				</a>
-			</div>
-				<?php perch_pages_navigation(array(
-				'hide-extensions' => true,
-				'levels'    => 1
-				)); ?>
+		<!-- Skip to main content link  -->
+		<a class="show-on-focus"  href="#main-content">Skip to main content</a>
+		<!-- For SVG logo. Delete if not needed -->
+		<?php perch_content("Hidden Logo"); ?>
+		<div class="main-logo-wrapper">
+			<a class="main-logo-link"  href="/">
+			<?php perch_content("Main Logo"); ?>
+		</a>
+	</div>
+	<?php perch_pages_navigation(array(
+		'hide-extensions' => true,
+		'levels'    => 1
+	)); ?>
 		</header>
 
