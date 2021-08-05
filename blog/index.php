08d7bf43348188228208d4a2008672168865d56c
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-05 15:13:39 +0100

    Cats template

--- blog/index.php
+++ blog/index.php
@@ -3,13 +3,11 @@
 <?php perch_layout('headers/blog-index-header'); ?>
   <main class="blog-main-content blog-index-main-content" id="main-content">
   <?php perch_content("Blog Index Heading"); ?>
-  <div class="blog-index-category-list">
       <?php
       $categories = perch_blog_categories(array(
-        'template' => 'category_link.html'
+        'template' => 'index_category_link.html'
       ));
       ?>
-    </div>
     <?php perch_blog_recent_posts(5); ?>
     <?php perch_content("Instagram Feed"); ?>
   </main>
