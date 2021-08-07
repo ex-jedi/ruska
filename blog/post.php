4cafb26a5ad88b7f9165dbf1141530e520f8ca7e
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-07 17:32:21 +0100

    Working previous and next blog articles

--- blog/post.php
+++ blog/post.php
@@ -1,41 +1,4 @@
 <?php include('../perch/runtime.php'); ?>
-
-<!-- Data for previous and next posts  -->
-<?php
-// get the post data, put it into $post variable
-$post = perch_blog_custom([
-  'filter'        => 'postSlug',
-  'match'         => 'eq',
-  'value'         => perch_get('s'),
-  'skip-template' => true,
-  'return-html'   => true
-]);
-
-// write the values from the array stored in $post to variables
-  $date  = $post[0]['postDateTime'];
-
-	$prev = perch_blog_custom([
-      'count'=>1,
-      'filter'=>'postDateTime',
-      'match'=>'lt',
-      'sort'=>'postDateTime',
-      'sort-order'=>'DESC',
-      'value'=>$date,
-      'template'=>'blog/post_prev.html'
-        ], true); // stores prev post in a variable to use later
-
-$next = perch_blog_custom([
-      'count'=>1,
-      'filter'=>'postDateTime',
-      'match'=>'gt',
-      'sort'=>'postDateTime',
-      'sort-order'=>'ASC',
-      'value'=>$date,
-      'template'=>'blog/post_next.html'
-      ], true); // stores next post in a variable to use later
-?>
-<!-- Data for previous and next posts  -->
-
 <?php perch_layout('heads/blog-post-head'); ?>
 <?php perch_layout('headers/blog-post-header'); ?>
 	<main class="blog-main-content blog-post-main-content" id="main-content">
@@ -44,16 +7,49 @@ $next = perch_blog_custom([
 				<?php perch_blog_post_categories(perch_get('s')); ?>
 				<?php perch_blog_post(perch_get('s')); ?>
 			</section>
-			<section class="blog-section featured-posts-section">
+			<section class="featured-posts-section body-text-medium">
+				<!-- Data for previous and next posts  -->
 				<?php
+				// get the post data, put it into $post variable
+				$post = perch_blog_custom([
+					'filter'        => 'postSlug',
+					'match'         => 'eq',
+					'value'         => perch_get('s'),
+					'skip-template' => true,
+					'return-html'   => true
+				]);
+
+				// write the values from the array stored in $post to variables
+					$date  = $post[0]['postDateTime'];
+
+					$next = perch_blog_custom([
+							'count'=>1,
+							'filter'=>'postDateTime',
+							'match'=>'lt',
+							'sort'=>'postDateTime',
+							'sort-order'=>'DESC',
+							'value'=>$date,
+							'template'=>'blog/post_next.html'
+								], true); // stores prev post in a variable to use later
+
+				$prev = perch_blog_custom([
+							'count'=>1,
+							'filter'=>'postDateTime',
+							'match'=>'gt',
+							'sort'=>'postDateTime',
+							'sort-order'=>'ASC',
+							'value'=>$date,
+							'template'=>'blog/post_prev.html'
+							], true); // stores next post in a variable to use later
+
 						// Previous and next post navigation
 						if (empty($prev)){
-						echo '<ul>';
+						echo '<p></p>';
 						} else {
 						echo $prev;
 						}
 						if (empty($next)){
-						echo '</ul>';
+						echo '<p></p>';
 						} else {
 						echo $next;
 						}
