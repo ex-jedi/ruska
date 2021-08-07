573bf4d9630e1fb750b274b6f649844c7df324e1
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-07 15:07:09 +0100

    Featured posts code in post page

--- blog/post.php
+++ blog/post.php
@@ -7,6 +7,28 @@
 				<?php perch_blog_post_categories(perch_get('s')); ?>
 				<?php perch_blog_post(perch_get('s')); ?>
 			</section>
+			<section class="blog-section featured-posts-section">
+				<?php
+					perch_blog_custom(array(
+					'sort'=>'postDateTime',
+					'sort-order'=>'RAND',
+					'template'=>'blog/featured-posts.html',
+					'count'=>3,
+					'filter-mode' => 'ungrouped',
+					'filter' => array(
+					array(
+					'filter' => 'postSlug',
+					'match' => 'neq',
+					'value' => perch_get('s'),
+					),
+					array(
+					'filter' => 'featured',
+					'match' => 'eq',
+					'value' => 'yes'
+					)
+					)
+				)); ?>
+			</section>
 		</article>
 		<?php perch_content("Contact Form"); ?>
 		<?php perch_content("Instagram Feed"); ?>
@@ -16,3 +38,4 @@
 <!-- TODO: Remove debug -->
 	<?php PerchUtil::output_debug(); ?>
 <!-- TODO: Validate HTML -->
+
