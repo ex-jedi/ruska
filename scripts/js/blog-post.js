5b96e913f645565cebf09d75fc3b5a2e472b3a68
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-07 16:34:01 +0100

    Blog desktop styles

--- scripts/js/blog-post.js
+++ scripts/js/blog-post.js
@@ -3,8 +3,10 @@
 // *==============================================================================
 
 import { mainLogoAnimation, addMenuListener, pyramidDividerFunction } from './animations';
-
+import { reframe } from './utils';
 // ********** Animations **********
 mainLogoAnimation();
 addMenuListener();
 pyramidDividerFunction();
+
+reframe('.blog-video-embed iframe');
