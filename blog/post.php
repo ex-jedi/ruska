ccd747c2c9d7eb1a3f06463ba245ac0747a420b6
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-07 14:14:00 +0100

    Favicon test

--- blog/post.php
+++ blog/post.php
@@ -7,31 +7,10 @@
 				<?php perch_blog_post_categories(perch_get('s')); ?>
 				<?php perch_blog_post(perch_get('s')); ?>
 			</section>
-			<section class="blog-section featured-posts-section">
-				<?php
-					perch_blog_custom(array(
-					'sort'=>'postDateTime',
-					'sort-order'=>'RAND',
-					'template'=>'blog/featured-posts.html',
-					'count'=>3,
-					'filter-mode' => 'ungrouped',
-					'filter' => array(
-					array(
-					'filter' => 'postSlug',
-					'match' => 'neq',
-					'value' => perch_get('s'),
-					),
-					array(
-					'filter' => 'featured',
-					'match' => 'eq',
-					'value' => 'yes'
-					)
-					)
-				)); ?>
-			</section>
 		</article>
 		<?php perch_content("Contact Form"); ?>
 		<?php perch_content("Instagram Feed"); ?>
 	</main>
 	<?php perch_layout('footers/main-footer'); ?>
 <?php perch_layout('footer-ends/blog-post-footer-end'); ?>
+<?php PerchUtil::output_debug(); ?>
