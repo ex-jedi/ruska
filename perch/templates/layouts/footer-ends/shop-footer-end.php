e0a7e3205fbf3275df8ec57033b95ee047d85994
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-16 16:46:26 +0100

    Basic shop starter page

---
+++ perch/templates/layouts/footer-ends/shop-footer-end.php
@@ -0,0 +1,4 @@
+<!-- JS file -->
+<script src="/scripts/minified/shop-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/shop-min.js');?>"></script>
+</body>
+</html>
---
+++ scripts/js/shop.js
@@ -0,0 +1,47 @@
+// *==============================================================================
+// ** Shop  **
+// *==============================================================================
+
+// TODO: Remove GSAP Imports
+import { gsap } from 'gsap';
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
+
+// *=========================================
+// ** Imports  **
+// *=========================================
+
+import { pyramidDividerFunction, mainLogoAnimation, scrollTriggerRefresh, addMenuListener } from './animations';
+
+import { displayInstagramFeed } from './utils';
+
+// *=========================================
+// ** GSAP  **
+// *=========================================
+gsap.registerPlugin(CSSRulePlugin, ScrollTrigger, DrawSVGPlugin);
+
+// !==============================================================================
+// FIXME: Temp Building 👇🏾
+// !==============================================================================
+
+// !==============================================================================
+// FIXME: Temp Building 👆🏾
+// !==============================================================================
+
+// *=========================================
+// ** Import Functions  **
+// *=========================================
+
+// ********** Animation **********
+
+mainLogoAnimation();
+
+pyramidDividerFunction();
+
+scrollTriggerRefresh();
+
+addMenuListener();
+
+// ********** Lib **********
+displayInstagramFeed();
