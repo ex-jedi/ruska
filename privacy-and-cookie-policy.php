34c883369502ed60acd5cd54563fa4958941ef45
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-16 14:39:20 +0100

    Terms page

--- privacy-and-cookie-policy.php
+++ privacy-and-cookie-policy.php
@@ -1,6 +1,6 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
-<?php perch_layout('main-head'); ?>
-<?php perch_layout('main-header'); ?>
+<?php perch_layout('heads/main-head'); ?>
+<?php perch_layout('headers/main-header'); ?>
 	<main class="legals privacy-policy" id="main-content">
 			<?php perch_content('Privacy And Cookie Policy'); ?>
 	</main>
