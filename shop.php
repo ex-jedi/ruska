4fa381e25111fd7a01d17a347fbb0fdcb4ee19bf
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-26 13:06:52 +0100

    Working list & detail pages

--- shop.php
+++ shop.php
@@ -2,7 +2,32 @@
 <?php perch_layout('heads/shop-head'); ?>
 <?php perch_layout('headers/main-header'); ?>
 <main class="main-content about-me-main-content" id="main-content">
-	<?php perch_content("Shop"); ?>
+		<!-- Create product region -->
+		<?php
+		perch_content_create('Products', array(
+		'template'   => 'product_detail.html',
+		'multiple'    => true,
+		'edit-mode' => 'listdetail',
+		));
+		?>
+		<!-- Display list or detail content  -->
+		<?php
+			if (perch_get('s')) {
+			// Detail mode
+				perch_content_custom('Products', array(
+					'template' => 'shop/product_detail.html',
+					'filter' => 'slug',
+					'match' => 'eq',
+					'value' => perch_get('s'),
+					'count' => 1,
+				));
+			} else {
+			// List mode
+			perch_content_custom('Products', array(
+						'template' => 'shop/product_listing.html',
+     ));
+			}
+		?>
 	<?php perch_content("Contact Form"); ?>
 	<?php perch_content("Instagram Feed"); ?>
 </main>
