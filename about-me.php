556462014b793f2068117f0cfb479d4ace710549
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-09-23 15:29:45 +0100

    Removing detail from listing page

--- about-me.php
+++ about-me.php
@@ -3,7 +3,7 @@
 <?php perch_layout('headers/main-header'); ?>
 <main class="main-content about-me-main-content" id="main-content">
 	<?php perch_content("Bio"); ?>
-	<?php perch_mailchimp_form('forms/subscribe'); ?>
+	<?php perch_content("Contact Form"); ?>
 	<?php perch_content("Instagram Feed"); ?>
 </main>
 <?php perch_layout('footers/main-footer'); ?>
