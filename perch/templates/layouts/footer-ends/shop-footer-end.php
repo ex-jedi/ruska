58ae2afaf05de290c40ff424aa3355ebb37cdd13
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-09-24 16:03:06 +0100

    Changing to prod SnipCart API

--- perch/templates/layouts/footer-ends/shop-footer-end.php
+++ perch/templates/layouts/footer-ends/shop-footer-end.php
@@ -1,7 +1,6 @@
 <!-- JS file -->
 <script src="/scripts/minified/shop-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/shop-min.js');?>"></script>
 <script async src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"></script>
-<div hidden id="snipcart" data-api-key="ZjRkMTljYzgtMDEyMy00NjI3LTk0NmUtYzNlMGEwYjY0NTRjNjM3NjQ3Mjc0MTIzNTI3Njg5
-"></div>
+<div hidden id="snipcart" data-api-key="YjBiMDdkYzItZGY0Yy00MzAwLTg4YWUtOWExNDVkMWI4OGI5NjM3NjgwOTE5MDcxNjM0MjE3"></div>
 </body>
 </html>
